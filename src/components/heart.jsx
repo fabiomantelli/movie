357c02de41f89855106ebf5a596744e03dc5aa74
Fábio Mantelli <fabiomantelli@Fabios-MacBook-Air.local>
2023-02-28 13:50:30 -0300

    feat: increment and dicrement buttons

---
+++ src/components/heart.jsx
@@ -0,0 +1,5 @@
+const Heart = () => {
+	return <i class='fa fa-heart' aria-hidden='true'></i>;
+};
+
+export default Heart;
---
+++ src/components/movies.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { getMovies } from '../services/fakeMovieService';
+import Heart from './heart';
+
+class Movies extends Component {
+	state = {
+		movies: getMovies(),
+	};
+
+	handleDelete = (movie) => {
+		const movies = this.state.movies.filter((m) => m._id !== movie._id);
+		this.setState({ movies });
+	};
+
+	render() {
+		const { length: count } = this.state.movies;
+		if (this.state.movies.length === 0)
+			return <p>There are no movies in the database</p>;
+
+		return (
+			<>
+				<p>Showing {count} movies in the database.</p>
+				<table className='table'>
+					<thead>
+						<tr>
+							<th scope='col'>Title</th>
+							<th scope='col'>Genre</th>
+							<th scope='col'>Stock</th>
+							<th scope='col'>Rate</th>
+							<th scope='col'></th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.movies.map((movie) => (
+							<tr key={movie._id}>
+								<td>{movie.title}</td>
+								<td>{movie.genre.name}</td>
+								<td>{movie.numberInStock}</td>
+								<td>{movie.dailyRentalRate}</td>
+								<td>
+									<Heart />
+								</td>
+								<td>
+									<button
+										className='btn btn-danger btn-sm'
+										onClick={() => this.handleDelete(movie)}
+									>
+										Delete
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</>
+		);
+	}
+}
+
+export default Movies;
